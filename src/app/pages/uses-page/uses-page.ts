5687595753d9a3225446c66e09f68b05136a4a98
Tanakorn Kaewmai <tanakorn488@outlook.com>
2026-01-13 09:48:06 +0700

    Update uses-page.ts

--- src/app/pages/uses-page/uses-page.ts
+++ src/app/pages/uses-page/uses-page.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
 import { SegmentedNavComponent } from '../../shared/components/segmented-nav-component/segmented-nav-component';
 
-import { FooterComponent } from '../home-page/components/footer-component/footer-component';
-
 @Component({
   selector: 'app-uses-page',
-  imports: [SegmentedNavComponent, FooterComponent],
+  imports: [SegmentedNavComponent],
   templateUrl: './uses-page.html',
   styleUrl: './uses-page.css',
 })
